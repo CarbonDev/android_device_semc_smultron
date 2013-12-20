e9dfaa33dc0d30b4344e540443137586f3de384e
vishal-android-freak <yzvishal.vd@gmail.com>
2013-12-20 21:09:58 +0530

    Carbon related changes
    Change-Id: I11d52c7dd63efafdb9a27e57562ed630a4fe8e22

--- full_smultron.mk
+++ full_smultron.mk
@@ -13,7 +13,7 @@
 # limitations under the License.
 
 # Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 
 # Inherit from smultron device
 $(call inherit-product, device/semc/smultron/smultron.mk)
@@ -28,3 +28,7 @@ PRODUCT_DEVICE := smultron
 PRODUCT_BRAND := SEMC
 PRODUCT_MANUFACTURER := SEMC
 PRODUCT_MODEL := Xperia Mini
+PRODUCT_PACKAGES += Apollo
+
+$(call inherit-product, device/semc/smultron/smultron.mk)
+$(call inherit-product-if-exists, vendor/semc/smultron/smultron-vendor.mk)
